90e2e08e7c183571b1dd594c81ad49e5c2c82b08
Maksym-Chmilov <maxchmilov@gmail.com>
2023-01-30 22:58:47 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { data } from 'data/data';
+import { MoviesGallery } from './MoviesGallery/MoviesGallery';
+import { Modal } from './Modal/Modal';
+const MOVIES_KEY = 'movies';
+
+export class App extends Component {
+  state = {
+    movies: data,
+    currentImg: null,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.movies !== this.state.movies) {
+      localStorage.setItem(MOVIES_KEY, JSON.stringify(this.state.movies));
+    }
+  }
+
+  componentDidMount() {
+    const localData = localStorage.getItem(MOVIES_KEY);
+
+    if (localData) {
+      this.setState({ movies: JSON.parse(localData) });
+    }
+  }
+
+  deleteMovie = id => {
+    this.setState(prevState => {
+      return { movies: prevState.movies.filter(movie => movie.id !== id) };
+    });
+  };
+
+  showPoster = data => {
+    this.setState({ currentImg: data });
+  };
+
+  render() {
+    const {currentImg} = this.state
+    return (
+      <>
+        <MoviesGallery
+          movies={this.state.movies}
+          deleteMovie={this.deleteMovie}
+          showPoster={this.showPoster}
+        />
+        {currentImg && <Modal currentImg={currentImg}/>}
+        
+      </>
+    );
+  }
+}
---
+++ src/components/MoviesGallery/MoviesGallery.jsx
@@ -0,0 +1,31 @@
+export function MoviesGallery({ movies, deleteMovie, showPoster }) {
+  //   const { id, title, vote_count: votes } = movies;
+  return (
+    <ul>
+      {movies.map(({ id, title, vote_count: votes, backdrop_path: img }) => {
+        return (
+          <li key={id}>
+            <h2>{title}</h2>
+            <p>Votes: {votes}</p>
+            <button
+              type="button"
+              onClick={() => {
+                deleteMovie(id);
+              }}
+            >
+              Delete
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                showPoster({src:{img}, alt:{title}});
+              }}
+            >
+              Open poster
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
